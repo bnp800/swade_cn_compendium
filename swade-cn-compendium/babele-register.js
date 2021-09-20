88684c998015c5771a8c9159bdb9f405d083e1fb
bnp800 <69395973+bnp800@users.noreply.github.com>
2021-09-20 11:43:39 +0800

    babele register

--- swade-cn-compendium/babele-register.js
+++ swade-cn-compendium/babele-register.js
@@ -2,7 +2,7 @@ Hooks.on('init', () => {
 
 	if(typeof Babele !== 'undefined') {
 		Babele.get().register({
-			module: 'swade_cn_compendium',
+			module: 'swade-cn-compendium',
 			lang: 'cn',
 			dir: 'compendium'
 		});
